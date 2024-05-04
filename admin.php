916729519ff333f0f9d757cd755acec8aeaf27fb
GUILHERME PEREIRA <guilhermemap85@gmail.com>
2024-05-03 22:24:06 -0300

    sistema de pokemon

---
+++ admin.php
@@ -0,0 +1,8 @@
+<?php include "cabecalho.php"; ?>
+
+<h2>Ambiente administrativo</h2>
+
+<!-- Cadastrar um novo pokemon -->
+<a href="form-inserir.php" class="btn btn-primary">NOVO POKEMON</a>
+
+<?php include "rodape.php"; ?>
